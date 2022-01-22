8f031b59f2e2d6261450e11b39d73519ac1b4e64
s-h-a-u-n-a <shauna.penistone@hotmail.com>
2022-01-22 16:48:33 +0000

    adding mob optimised navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react'
-
 import { Gradient } from 'react-gradient';
-
 import styled from 'styled-components';
+import Navbar from './components/Navbar';
+import {
+  BrowserRouter,
+  Routes, // instead of "Switch"
+  Route,
+} from "react-router-dom";
+
+import LandingPage from './pages/LandingPage'
 
 const gradients = [
   ['#CEB9EC', '#FFA4F3'], // lilac -> pink
@@ -11,24 +17,27 @@ const gradients = [
 
 const View = styled.div`
   width: 100%;
-  min-height: 120vh;
+  min-height: 100vh;
   height: 100%
 `
 
 function App() {
   return (
-    <>
       <Gradient
         gradients={ gradients }
         property="background"
         duration={ 5000 }
         angle="90deg"
       >    
+        <BrowserRouter>
         <View>
-        <h1>test</h1>
+          <Navbar />
+          <Routes>
+            <Route path="*" component={LandingPage} />
+          </Routes>
         </View>
+        </BrowserRouter>
       </Gradient>  
-    </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { FiMenu } from 'react-icons/fi'
+import { BsFillTriangleFill } from 'react-icons/bs';
+import { HashLink } from 'react-router-hash-link';
+import './Navbar.css';
+
+function Navbar() {
+  const [ click, setClick ] = useState(false);
+
+  const handleClick = () => setClick(!click)
+  const closeMobileMenu = () => setClick(false);
+
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          {/* <HashLink smooth to={"/home"} className="navbar-logo">
+            home
+          </HashLink> */}
+          <div class="menu-icon" onClick={handleClick}>
+            {click ? 
+              <BsFillTriangleFill color='pink' size='35px'/> 
+              : <FiMenu color='pink' size='35px'/> 
+            }
+          </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <HashLink smooth to={"/"} className='nav-links' >
+                Portfolio
+              </HashLink>
+            </li>
+            <li className='nav-item'>
+              <HashLink smooth to={"/"} className='nav-links' onClick={closeMobileMenu}>
+                Contact Me
+              </HashLink>
+            </li>
+            <li className='nav-item'>
+              <HashLink smooth to={"/"} className='nav-links' onClick={closeMobileMenu}>
+                Experience
+              </HashLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
